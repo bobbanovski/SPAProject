7fc0cf308c52474dd6878a3b99b883c1b76d3c68
DevOps\Developer <bobbanovski@gmail.com>
2017-01-14 21:44:05 +1100

    Extract user details from example api
    using http://jsonplaceholder.typicode.com/
    as example api

--- app/users.component.js
+++ app/users.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', 'angular2/http', 'rxjs/add/operator/map', './user.service'], function(exports_1, context_1) {
+System.register(['angular2/core', 'rxjs/add/operator/map', './user.service'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,15 +10,12 @@ System.register(['angular2/core', 'angular2/http', 'rxjs/add/operator/map', './u
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, http_1, user_service_1;
+    var core_1, user_service_1;
     var UsersComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
-            },
-            function (http_1_1) {
-                http_1 = http_1_1;
             },
             function (_1) {},
             function (user_service_1_1) {
@@ -26,14 +23,20 @@ System.register(['angular2/core', 'angular2/http', 'rxjs/add/operator/map', './u
             }],
         execute: function() {
             UsersComponent = (function () {
-                function UsersComponent() {
+                function UsersComponent(_service) {
+                    this._service = _service;
                 }
+                UsersComponent.prototype.ngOnInit = function () {
+                    var _this = this;
+                    this._service.getUsers()
+                        .subscribe(function (users) { return _this.users = users; });
+                };
                 UsersComponent = __decorate([
                     core_1.Component({
-                        template: "\n        <h1>Users</h1>\n        <table class=\"table table-bordered\">\n            <thead>\n                <tr>\n                    <th>Name</th>\n                    <th>Email</th>\n                    <th>Edit</th>\n                    <th>Delete</th>\n                </tr>\n            </thead>\n            \n        </table>\n    ",
-                        providers: [user_service_1.UserService, http_1.HTTP_PROVIDERS]
+                        template: "\n        <h1>Users</h1>\n        <table class=\"table table-bordered\">\n            <thead>\n                <tr>\n                    <th>Name</th>\n                    <th>Email</th>\n                    <th>Edit</th>\n                    <th>Delete</th>\n                </tr>\n            </thead>\n            <tbody>\n                <tr *ngFor=\"#user of users\">\n                    <td>{{ user.name}}</td>\n                    <td>{{ user.email }}</td>\n                    <td><i class=\"glyphicon glyphicon-edit\"></i></td>\n                    <td><i class=\"glyphicon glyphicon-remove\"></i></td>\n                </tr>\n            </tbody>\n        </table>\n    ",
+                        providers: [user_service_1.UserService]
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [user_service_1.UserService])
                 ], UsersComponent);
                 return UsersComponent;
             }());
--- app/users.component.ts
+++ app/users.component.ts
@@ -3,7 +3,6 @@ import {HTTP_PROVIDERS} from 'angular2/http';
 import 'rxjs/add/operator/map';
 import {UserService} from './user.service';
 import {Observable} from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
 
 @Component({
     template: `
@@ -17,11 +16,25 @@ import 'rxjs/add/operator/map';
                     <th>Delete</th>
                 </tr>
             </thead>
-            
+            <tbody>
+                <tr *ngFor="#user of users">
+                    <td>{{ user.name}}</td>
+                    <td>{{ user.email }}</td>
+                    <td><i class="glyphicon glyphicon-edit"></i></td>
+                    <td><i class="glyphicon glyphicon-remove"></i></td>
+                </tr>
+            </tbody>
         </table>
     `,
-    providers: [UserService, HTTP_PROVIDERS]
+    providers: [UserService]
 })
-export class UsersComponent{
+export class UsersComponent implements OnInit {
+    users: any[];
     
+    constructor(private _service: UserService){}
+
+    ngOnInit(){
+        this._service.getUsers()
+            .subscribe(users => this.users = users);
+    }
 }
